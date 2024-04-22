27d35fa204df36da0ea282a8c0ea099448398527
SOG-gen <raheemolalekanusman84@gmail.com>
2024-04-22 03:01:15 +0100

    reka angular editor added more clean up

--- projects/ngx-reka-editor/src/lib/ngx-reka-editor.component.ts
+++ projects/ngx-reka-editor/src/lib/ngx-reka-editor.component.ts
@@ -1,16 +1,15 @@
-import {Component, ElementRef, Input, OnDestroy, OnInit, signal, ViewChild} from '@angular/core';
-import { EditorState, Extension } from '@codemirror/state';
-import { EditorView, keymap } from '@codemirror/view';
-import { reka as rekaCodemirrorExtension } from '@rekajs/codemirror';
-import { basicSetup } from 'codemirror';
+import {AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, signal, ViewChild} from '@angular/core';
+import {EditorState, Extension} from '@codemirror/state';
+import {EditorView, keymap} from '@codemirror/view';
+import {reka as rekaCodemirrorExtension} from '@rekajs/codemirror';
+import {basicSetup} from 'codemirror';
 import {NgxRekaEditorService} from "./ngx-reka-editor.service";
-import {Subject, Subscription, takeUntil} from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
+import {debounceTime, Subject, Subscription, takeUntil} from 'rxjs';
 import {onStatusChangeCb, ParserStatus} from "./utils";
-import { NgxRekaService } from 'ngx-reka';
 import * as t from '@rekajs/types';
 import {Parser} from "@rekajs/parser";
 import {Reka} from "@rekajs/core";
+import {error} from "ng-packagr/lib/utils/log";
 
 @Component({
   selector: 'ngx-reka-editor',
@@ -27,7 +26,7 @@ import {Reka} from "@rekajs/core";
     }
   `]
 })
-export class NgxRekaEditorComponent implements OnInit, OnDestroy {
+export class NgxRekaEditorComponent implements OnInit, OnDestroy, AfterViewInit {
   @ViewChild('editorRef', { static: true }) editorRef!: ElementRef;
   @Input() extensions: Extension[] = [];
   @Input({required: true})  reka!: Reka;
@@ -35,21 +34,28 @@ export class NgxRekaEditorComponent implements OnInit, OnDestroy {
   private codemirrorView: EditorView | null = null;
   private codeChangeSubject = new Subject<string>();
   private statusChangeSubject = new Subject<ParserStatus>();
-
+  private currentSate = signal<any | null>(null);
+  private isSynchingFromCodeMirror = signal(false);
+  private isSynchingFromExternal = signal(false);
+  private isTyping = signal(false);
+  private currentCodeString = signal<any | null>(null);
+//
   public getOnStatusChange(callback: onStatusChangeCb): Subscription {
     return this.statusChangeSubject.asObservable().subscribe(callback);
   }
 
   private destroy$ = new Subject<void>();
+  private unsubscribe$: any;
 
   constructor(private rekaEitordService: NgxRekaEditorService) {}
 
   ngOnInit() {
     const editorDom = this.editorRef.nativeElement;
-
-    const view = new EditorView({
+    this.currentCodeString.set(Parser.stringify(this.reka.program));
+    this.currentSate.set(t.Schema.fromJSON(this.reka.program));
+    this.codemirrorView = new EditorView({
       state: EditorState.create({
-        doc: Parser.stringify(this.reka.program),
+        doc: this.currentCodeString(),
         extensions: [
           basicSetup,
           keymap.of([]), // Add your keymaps if needed
@@ -71,12 +77,20 @@ export class NgxRekaEditorComponent implements OnInit, OnDestroy {
               color: 'rgba(0,0,0,0.4)',
             },
           }),
+          EditorView.updateListener.of((view) => {
+            if (!view.docChanged || this.isSynchingFromCodeMirror()) {
+              return;
+            }
+
+            this.isTyping.set(true);
+            this.currentCodeString.set(view.state.doc.toString());
+            this.onStatusChange({type: 'parsing'})
+            this.codeChangeSubject.next(this.currentCodeString());
+          }),
         ],
       }),
       parent: editorDom,
     });
-
-    this.codemirrorView = view;
 
     this.codeChangeSubject.pipe(
       debounceTime(1000),
@@ -84,33 +98,24 @@ export class NgxRekaEditorComponent implements OnInit, OnDestroy {
     ).subscribe({
       next: code => this.handleCodeChange(code),
       error: error => this.onStatusChange({ type: 'error', error: error.message })
-  });
-
-    // Listen to changes in the editor
-    view.dom.addEventListener('keydown', () => {
-      const code = view.state.doc.toString();
-      this.codeChangeSubject.next(code);
     });
   }
 
   private handleCodeChange(code: string) {
-    try {
-      this.handleProgramChange(code, this.reka.program)
-    } catch (error) {
-      // @ts-ignore
-      this.onStatusChange({ type: 'error', error: error.message });
-    }
-  }
 
-  private handleProgramChange(code: string, program: t.Program | null) {
-    if (program) {
+    if(this.isSynchingFromExternal()) {
+      return;
+    }
+    this.isSynchingFromCodeMirror.set(true);
+    if (this.reka.program) {
       this.onStatusChange({ type: 'parsing' })
       try {
         const newAST = this.rekaEitordService.parseProgram(code);
-        // if (JSON.stringify(newAST) !== JSON.stringify(program)) {
-          this.rekaEitordService.diffAST(program, newAST);
-          this.onStatusChange({ type: 'success' });
-        // }
+        this.reka.change(() => {
+          this.rekaEitordService.diffAST(this.currentSate(), newAST);
+          this.rekaEitordService.diffAST(this.reka.program, this.currentSate());
+        });
+        this.onStatusChange({ type: 'success' });
       } catch (error) {
         // @ts-ignore
         this.onStatusChange({ type: 'error', error: error.message });
@@ -118,6 +123,8 @@ export class NgxRekaEditorComponent implements OnInit, OnDestroy {
     } else {
       this.onStatusChange({ type: 'error', error: 'Reka -> Program is not available' });
     }
+    this.isSynchingFromCodeMirror.set(false);
+    this.isTyping.set(false);
   }
 
   private onStatusChange(status: ParserStatus) {
@@ -125,22 +132,41 @@ export class NgxRekaEditorComponent implements OnInit, OnDestroy {
   }
 
   public onExternalChange() {
-    try {
-      if (this.codemirrorView) {
-        const newCode = this.rekaEitordService.stringifyProgram( this.reka.program);
-        // const currentCode = this.codemirrorView.state.doc.toString();
 
-        // if (newCode !== currentCode) {
-        const transaction = this.codemirrorView.state.update({
-          changes: {
-            from: 0,
-            to: this.codemirrorView.state.doc.length,
-            insert: newCode,
-          },
-        });
-        this.codemirrorView.dispatch(transaction);
-        // }
+    if(this.isSynchingFromCodeMirror() || this.isTyping()) {
+      return
+    }
+
+    if(this.isSynchingFromExternal()) {
+      return;
+    }
+
+    try {
+      if(!this.codemirrorView) {
+        return;
       }
+
+      const newCode = this.rekaEitordService.stringifyProgram( this.reka.program);
+
+      if(newCode === this.currentCodeString()) {
+        this.isSynchingFromExternal.set(false)
+        return;
+      }
+
+      this.currentSate.set(t.Schema.fromJSON(this.reka.program))
+
+      const transaction = this.codemirrorView.state.update({
+        changes: {
+          from: 0,
+          to: this.codemirrorView.state.doc.length,
+          insert: newCode,
+        },
+      });
+
+      this.currentCodeString.set(newCode);
+      this.codemirrorView.dispatch(transaction);
+      this.isSynchingFromExternal.set(false);
+
     } catch (error) {
       // @ts-ignore
       this.onStatusChange({type: 'error', error: error.message})
@@ -154,5 +180,15 @@ export class NgxRekaEditorComponent implements OnInit, OnDestroy {
     if (this.codemirrorView) {
       this.codemirrorView.destroy();
     }
+
+    if(this.unsubscribe$) {
+      this.unsubscribe$()
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.unsubscribe$ = this.reka.listenToChangeset(() => {
+      this.onExternalChange();
+    })
   }
 }
